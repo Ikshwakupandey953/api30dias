7864678f050de3bca671f6b992e9b71274f0f3c1
oesiXD <39572329+oesiXD@users.noreply.github.com>
2022-04-11 10:17:03 -0400

    port
    change

--- app.js
+++ app.js
@@ -152,6 +152,6 @@ app.post('/sendmessage', async (req, res, next) => {
   
 
   // Listening for the server
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3001;
   app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
   
